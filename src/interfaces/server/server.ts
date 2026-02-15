9633241df9a246cc874bd1e6ea05cd731e10045e
maxy <max.yevdokimov@gmail.com>
2026-02-15 18:56:06 +0100

    [main-log] setOnError

--- src/interfaces/server/server.ts
+++ src/interfaces/server/server.ts
@@ -11,24 +11,3 @@ export declare function Server(
   target: new (preset?: any) => object,
   context: ClassDecoratorContext,
 ) => void;
-
-interface ValidationError {
-    type: string;
-    expected?: unknown;
-    value?: unknown;
-    path: (string | number)[];
-}
-
-interface HttpError extends Error {
-    status: number;
-    type?: string;
-    code?: string;
-    errors?: ValidationError[];
-    constraint?: string;
-    table?: string;
-    detail?: string;
-}
-
-type OnErrorHandler = (error: HttpError, status: number, context: ServerContext) => void;
-
-export declare function setOnError(fn: OnErrorHandler): void;
